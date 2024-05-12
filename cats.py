9c505560d8a9f85412a9f44c97b4c54eb42dac0d
Evgeny Volosatov <75351473+EVolosatov@users.noreply.github.com>
2024-05-12 13:39:09 +0700

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Создание изображения
+img = np.zeros((500, 500, 3), dtype=np.uint8)
+for i in range(10):
+ img = add_noise(img)
+
+# Отображение изображения
+plt.imshow(img)
+plt.axis('off')
+plt.title('Котики')
+plt.show()
